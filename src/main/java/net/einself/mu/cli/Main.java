fd3d6cc7bcf94ee9ec4f21557e4ecc634d8c7857
Pierre Klink <dev@einself.net>
2026-08-05 13:57:09 +0200

    style: reformat Main.java for improved readability

--- src/main/java/net/einself/mu/cli/Main.java
+++ src/main/java/net/einself/mu/cli/Main.java
@@ -41,7 +41,7 @@ public class Main implements Callable<Integer> {
 
     private CommandLine commandLine;
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         System.exit(execute(args, System.out, System.err));
     }
 
