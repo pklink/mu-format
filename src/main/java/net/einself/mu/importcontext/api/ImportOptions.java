2be2addd5bceb8189d312f243915d51fc8d2909b
Pierre Klink <dev@einself.net>
2026-08-31 17:12:17 +0200

    refactor: remove unused ImportOptions builder methods

--- src/main/java/net/einself/mu/importcontext/api/ImportOptions.java
+++ src/main/java/net/einself/mu/importcontext/api/ImportOptions.java
@@ -9,23 +9,4 @@ public record ImportOptions(
                                 boolean origin,
                                 @Nullable String artistId,
                                 @Nullable String releaseId) {
-    public static ImportOptions defaults() {
-        return new ImportOptions(false, false, null, null);
-    }
-
-    public ImportOptions withDryRun(boolean dryRun) {
-        return new ImportOptions(dryRun, origin, artistId, releaseId);
-    }
-
-    public ImportOptions withOrigin(boolean origin) {
-        return new ImportOptions(dryRun, origin, artistId, releaseId);
-    }
-
-    public ImportOptions withArtistId(@Nullable String artistId) {
-        return new ImportOptions(dryRun, origin, artistId, releaseId);
-    }
-
-    public ImportOptions withReleaseId(@Nullable String releaseId) {
-        return new ImportOptions(dryRun, origin, artistId, releaseId);
-    }
 }
